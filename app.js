de7af1ce9af41a8df507a4a0b5893f6cdcb2e614
Tomas Agustin <96827491+tpCodeX@users.noreply.github.com>
2023-01-06 07:09:24 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'}
+        
+        });
+});
